2b9276db50cc03a7a1bd85bd31b63a19afcc2602
rhysmoggs <rhysmoggs@hotmail.com>
2022-06-11 13:05:02 +0000

    setup mongo db and link environment variables

--- cocktails/__init__.py
+++ cocktails/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
@@ -8,7 +9,12 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
+
 
 from cocktails import routes  # noqa
--- cocktails/routes.py
+++ cocktails/routes.py
@@ -1,8 +1,13 @@
-from flask import render_template
-from cocktails import app, db
+from flask import (
+    flash, render_template,
+    request, redirect, session, url_for)
+from bson.objectid import ObjectId
+from cocktails import app, db, mongo
 from cocktails.models import Category, Users
 
 
 @app.route("/")
-def home():
-    return render_template("base.html")
+@app.route("/get_cocktails")
+def get_cocktails():
+    cocktails = list(mongo.db.cocktails.find())
+    return render_template("cocktails.html", cocktails=cocktails)
