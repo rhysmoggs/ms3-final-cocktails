ccafba9b1da093e76dc799f7b7a5be39a95e537e
rhysmoggs <rhysmoggs@hotmail.com>
2022-06-13 11:17:58 +0000

    Update __init__.py file

--- cocktails/__init__.py
+++ cocktails/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
@@ -8,9 +9,13 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 
 db = SQLAlchemy(app)
