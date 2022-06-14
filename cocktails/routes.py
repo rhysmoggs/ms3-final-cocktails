dc380d85bef9546576a30bb30c3ca114155c9167
rhysmoggs <rhysmoggs@hotmail.com>
2022-06-14 12:26:59 +0000

    Add ability to logout

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -81,6 +81,14 @@ def profile(username):
     return redirect(url_for("login"))
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookie
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
 @app.route("/categories")
 def categories():
     categories = list(Category.query.order_by(Category.category_name).all())
