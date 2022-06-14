410804f0c31d093156a43e5734add0c8392d379f
rhysmoggs <rhysmoggs@hotmail.com>
2022-06-14 11:32:55 +0000

    Add navbar links and update to style.css and script.js

--- cocktails/static/js/script.js
+++ cocktails/static/js/script.js
@@ -1,11 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);
-    // select initialization
-    let selects = document.querySelectorAll('select');
-    M.FormSelect.init(selects);
-    // collapsible popout initialization
-    let collapsibles = document.querySelectorAll('.collapsible');
-    M.Collapsible.init(collapsibles);
+$(document).ready(function(){
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
