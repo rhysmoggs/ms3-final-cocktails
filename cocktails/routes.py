c0c5231866716edfc4e14cf07580bb5405ab24a0
rhysmoggs <rhysmoggs@hotmail.com>
2022-07-05 17:39:17 +0000

    Add ability to add and remove other ingredients to cocktails

--- cocktails/routes.py
+++ cocktails/routes.py
@@ -209,7 +209,7 @@ def edit_cocktail(cocktail_id):
     cocktail = mongo.db.cocktails.find_one({"_id": ObjectId(cocktail_id)})
 
     if "user" not in session or session["user"] != cocktail["created_by"]:
-        flash("You can only remix your own cocktails!")
+        flash("You can only edit your own cocktails!")
         return redirect(url_for("all_cocktails"))
 
     if request.method == "POST":
--- cocktails/static/js/script.js
+++ cocktails/static/js/script.js
@@ -34,3 +34,33 @@ $(document).ready(function(){
         });
     }
 });
+
+
+// add and remove other ingredients
+let new_input = document.getElementById('new-input');
+let add_other_btn = document.getElementById('add-other-btn');
+let remove_other_btn = document.getElementById('remove-other-btn');
+
+add_other_btn.onclick = function() {
+    var newIngredient = document.createElement('input');
+    newIngredient.setAttribute('name', 'other_ingredient');
+    newIngredient.setAttribute('minlength', '2');
+    newIngredient.setAttribute('max-length', '20');
+    newIngredient.setAttribute('class', 'js-remove-test remove-other-btn validate');
+    newIngredient.setAttribute('type', 'text');
+    newIngredient.setAttribute('placeholder', 'Add more ingredients and measurements');
+    newIngredient.required = true;
+    new_input.appendChild(newIngredient);
+    remove_other_btn.classList.remove("hide")
+};
+
+remove_other_btn.onclick = function() {
+    var input_count = new_input.getElementsByClassName('js-remove-test');
+    var btn_count = new_input.getElementsByClassName('remove-other-btn');
+    if(input_count.length > 1) {
+        new_input.removeChild(input_count[(input_count.length) - 1]);
+        if(input_count.length = 1 && btn_count.length < 1) {
+            remove_other_btn.classList.add("hide");
+        };
+    }
+};
